9e2abfb35e586d5a25d96b50db912d6090163f86
Michael Dougherty <maackle.d@gmail.com>
2015-12-07 15:27:03 -0800

    config

--- config/production.py
+++ config/production.py
@@ -1,6 +1,1 @@
 SECRET_KEY = 'LKSDHF*(*(98GG87W]\|}|CNJ'
-
-MONGODB_SETTINGS = {
-	'DB': 'astrololo',
-	'host': 'mongodb://heroku_app25898889:kpnuk46pflvc0v9q73l9igar8@ds051378.mongolab.com:51378/heroku_app25898889'
-}
